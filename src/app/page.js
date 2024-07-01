6778ffd35ede4c6023ab311fd1e2347568be310e
arjit.singh <arjit.singh@extramarks.com>
2024-07-01 10:29:33 +0530

    added images

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,17 @@
 import LoginForm from "./components.js/LoginForm";
 import './styles/globals.scss'
+import Image from 'next/image';
+import logo from './images/wtvision-image.png'
 
 export default function Home() {
   return (
-    <div className="signup-container"><LoginForm /></div>
+    <>
+      <div className="wtvision-logo">
+        <Image src={logo} alt="wtvision" />
+      </div>
+      <div className="signup-container">
+        <LoginForm />
+      </div>
+    </>
   );
 }
